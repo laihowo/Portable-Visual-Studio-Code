76d4cfa787a70ab3198db3b3105d24e838cecc99
Benny Lai <howo0707@gmail.com>
2021-06-24 00:11:21 +0800

    Update icode.sh

--- icode.sh
+++ icode.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-IP=$(gcloud cloud-shell ssh --dry-run --authorize-session | grep -o "[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+")
+IP=$(./gcloud-sdk/gcloud cloud-shell ssh --dry-run --authorize-session | grep -o "[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+")
 if [ -z "$IP" ]
 then
     IP=1.2.3.4
 fi
-#echo $IP
+echo $IP
 sed -i "s/[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+/$IP/" .ssh/config
