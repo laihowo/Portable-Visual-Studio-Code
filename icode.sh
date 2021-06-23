184eaa5ef6f1d2524b95d707d99833041eafad3a
Benny Lai <howo0707@gmail.com>
2021-06-24 00:09:23 +0800

    Add files via upload

---
+++ icode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+IP=$(gcloud cloud-shell ssh --dry-run --authorize-session | grep -o "[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+")
+if [ -z "$IP" ]
+then
+    IP=1.2.3.4
+fi
+#echo $IP
+sed -i "s/[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+/$IP/" .ssh/config
