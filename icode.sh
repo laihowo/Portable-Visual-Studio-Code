8a81e92fa49eaf24fc978a80cd11aad6bc9cbc98
Benny Lai <howo0707@gmail.com>
2021-06-25 16:46:56 +0800

    Update icode.sh

--- icode.sh
+++ icode.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Mac
+# Mac Visual Studio Code
+ZIP=VSCode-darwin-universal.zip
+curl -O "https://az764295.vo.msecnd.net/stable/507ce72a4466fbb27b715c3722558bb15afa9f48/$ZIP"
 # wget "https://code.visualstudio.com/sha/download?build=stable&os=darwin-universal"
-# open
+unzip $ZIP
+open Visual\ Studio\ Code.app/
 
 # gcloud
-# curl -O "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-346.0.0-linux-x86_64.tar.gz"
-# tarf -vf
-# ./google-cloud-sdk/bin/gcloud init
+TAR=google-cloud-sdk-346.0.0-linux-x86_64.tar.gz
+curl -O "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$TAR"
+tar -xzvf $TAR
+./google-cloud-sdk/bin/gcloud init
 
 IP=$(./google-cloud-sdk/bin/gcloud cloud-shell ssh --dry-run --authorize-session | grep -o "[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+")
 if [ -z "$IP" ]
