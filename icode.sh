5ed228e0594b93bfc5bbd5c8600759d74266433c
Benny Lai <howo0707@gmail.com>
2021-06-28 17:49:44 +0800

    Update icode.sh

--- icode.sh
+++ icode.sh
@@ -9,3 +9,10 @@ fi
 
 sed -Ei "" "s/[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+/$IP/" config
 cp config /Users/library/.ssh/config
+
+# Mac Visual Studio Code
+# ZIP=VSCode-darwin-universal.zip
+# curl -O "https://az764295.vo.msecnd.net/stable/507ce72a4466fbb27b715c3722558bb15afa9f48/$ZIP"
+# wget "https://code.visualstudio.com/sha/download?build=stable&os=darwin-universal"
+# unzip $ZIP
+open Visual\ Studio\ Code.app/
