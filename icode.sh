7098bed6d0d368060ea524c42d3efcbf4e1d5fad
Benny Lai <howo0707@gmail.com>
2021-06-28 16:51:32 +0800

    Update icode.sh

--- icode.sh
+++ icode.sh
@@ -5,7 +5,7 @@ if [ -z "$IP" ]
 then
     IP=1.2.3.4
 fi
-echo $IP
-# sed -i "s/[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+/$IP/" .ssh/config
+#echo $IP
 
+sed -Ei "" "s/[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+/$IP/" config
 cp config /Users/library/.ssh/config
