f7227d8b9dcc87d2100b24ecc1667833ebbbee41
Benny Lai <howo0707@gmail.com>
2021-06-28 21:04:59 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@
 # Mac Google Chrome
 open -a "Google Chrome"
 
+# Mac Visual Studio Code
+./Visual_Studio_Code.app/Contents/Resources/app/bin/code --install-extension ms-vscode-remote.remote-ssh
+
 # gcloud
 # TAR=google-cloud-sdk-346.0.0-linux-x86_64.tar.gz
 # curl -O "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$TAR"
