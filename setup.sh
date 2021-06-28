78c35c5fd15aca8d7f22a9093eabc21ac02281a9
Benny Lai <howo0707@gmail.com>
2021-06-28 20:46:52 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 
 # cd Portable-Visual-Studio-Code
 
+# Mac Google Chrome
+open -a "Google Chrome"
+
 # gcloud
 # TAR=google-cloud-sdk-346.0.0-linux-x86_64.tar.gz
 # curl -O "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$TAR"
 # tar -xzvf $TAR
 ./google-cloud-sdk/bin/gcloud init
-
-# Mac Google Chrome
-open -a "Google Chrome"
